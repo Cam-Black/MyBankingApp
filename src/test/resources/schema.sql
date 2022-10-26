7ded1db2ec0b1bceaec1b1c498ba00524c2e0074
Cam-Black <cameronablack1998@gmail.com>
2022-10-26 14:58:16 +0100

    Changed Decimal to Double.

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,11 +1,9 @@
-CREATE SCHEMA IF NOT EXISTS test;
-USE test;
 DROP TABLE IF EXISTS transactions;
 CREATE TABLE IF NOT EXISTS transactions
 (
     `id`               BIGINT AUTO_INCREMENT PRIMARY KEY,
     `transaction_date` DATE,
     `vendor`           varchar(255) NOT NULL,
-    `amount`           DECIMAL      NOT NULL,
+    `amount`           DOUBLE       NOT NULL,
     `category`         varchar(255)
 );
