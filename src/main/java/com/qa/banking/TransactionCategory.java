ccce224b7dca488f324cbe9d68456c43d3fb0896
Cam-Black <cameronablack1998@gmail.com>
2022-10-27 13:10:34 +0100

    Implemented working code to get the average spend in a month for a given category.

--- src/main/java/com/qa/banking/TransactionCategory.java
+++ src/main/java/com/qa/banking/TransactionCategory.java
@@ -54,8 +54,8 @@ public class TransactionCategory {
 	
 	public List<Transaction> getAllTransactionsByCategory(String category) {
 		try (Connection conn = DBUtils.getInstance().getConnection();
-			PreparedStatement ps = conn.prepareStatement(
-					"SELECT * FROM transactions where category = ? order by transaction_date desc;")) {
+		     PreparedStatement ps = conn.prepareStatement(
+				     "SELECT * FROM transactions where category = ? order by transaction_date desc;")) {
 			List<Transaction> transactions = new ArrayList<>();
 			ps.setString(1, category);
 			ResultSet rs = ps.executeQuery();
@@ -81,6 +81,19 @@ public class TransactionCategory {
 	}
 	
 	public double getAvgSpendInAMonth(String category) {
-		return 150.0;
+		try (Connection conn = DBUtils.getInstance().getConnection();
+		     PreparedStatement ps = conn.prepareStatement(
+				     "SELECT AVG(amount) FROM transactions WHERE category = ? AND  MONTH(transaction_date) = MONTH" +
+						     "(CURRENT_DATE()) AND YEAR(transaction_date) = YEAR (CURRENT_DATE());")) {
+			ps.setString(1, category);
+			ResultSet rs = ps.executeQuery();
+			
+			if (rs.next()) {
+				return rs.getDouble(1);
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+		return 0.0;
 	}
 }
