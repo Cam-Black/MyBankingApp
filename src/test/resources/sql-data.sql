a1c785015fc2abe8428421445b7c02e00a7c8b8a
Cam-Black <cameronablack1998@gmail.com>
2022-10-27 12:17:58 +0100

    Added test data to check avg spend in a month.

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,2 +1,7 @@
 INSERT INTO `transactions` (`transaction_date`, `vendor`, `amount`, `category`)
-VALUES ('2022-03-15', 'WHSmiths', 10.50, 'Leisure'), ('2022-05-16', 'Co-Op', '10.50', 'Groceries'), ('2022-08-21', 'Tesco', '10.30', 'Groceries');
+VALUES ('2022-03-15', 'WHSmiths', 10.50, 'Leisure'),
+       ('2022-05-16', 'Co-Op', '10.50', 'Groceries'),
+       ('2022-08-21', 'Tesco', '10.30', 'Groceries'),
+       ('2022-06-04', 'Car Finance', '200.00', 'Direct Debit'),
+       ('2022-05-04', 'Car Finance', '212.43', 'Direct Debit'),
+       ('2022-06-01', 'Credit Card', '100.00', 'Direct Debit');
