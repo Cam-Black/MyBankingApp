8614e29a88898fc673c604a96c74d78969759d06
Cam-Black <cameronablack1998@gmail.com>
2022-10-27 10:40:51 +0100

    Renamed to sql-data.sql

--- src/test/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `transactions` (`transaction_date`, `vendor`, `amount`, `category`)
-VALUES ('2022-03-15', 'WHSmiths', 10.50, 'Leisure'),
-       ('2022-05-16', 'Co-Op', '10.50', 'Groceries'),
-       ('2022-05-16', 'Tesco', '10.30', 'Groceries');
